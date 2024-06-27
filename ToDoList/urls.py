a267dd6a880f3dac3de38565618087583d4fcc85
HeshamAbedelatty <117268381+HeshamAbedelatty@users.noreply.github.com>
2024-06-27 06:04:11 +0300

    alter todolist on get

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import ToDoTaskView, ToDoTaskDetailView, ToDoListView, ToDoListDetailView, ToDoListDetailsView
 
 urlpatterns = [
-    path('', ToDoListDetailsView.as_view(), name='todolist-list'),
+    path('', ToDoListView.as_view(), name='todolist-list'),
     path('<int:pk>/', ToDoListDetailView.as_view(), name='todolist-detail'),
     path('<int:todolist_pk>/tasks/', ToDoTaskView.as_view(), name='todotask-list'),
     path('<int:todolist_pk>/tasks/<int:pk>/', ToDoTaskDetailView.as_view(), name='todotask-detail'),
-    # path('<int:todolist_pk>/tasks/<int:pk>/', ToDoListDetailsView.as_view(), name='todotask-details'),
+    path('listall/', ToDoListDetailsView.as_view(), name='todotask-details'),
 ]
